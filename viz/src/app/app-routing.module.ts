99cb3f4b660da6a181577d51b3ddc76863c9c7be
Kovács Csaba <0tac-kovcsa@MORICZREF.LAN>
2024-12-13 12:12:01 +0100

    kapcsolat

--- viz/src/app/app-routing.module.ts
+++ viz/src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { HibaFelvitelComponent } from './hiba-felvitel/hiba-felvitel.component';
 import { HibaListaComponent } from './hiba-lista/hiba-lista.component';
 import { ChatComponent } from './chat/chat.component';
+import { KapcsolatComponent } from './kapcsolat/kapcsolat.component';
 
 const routes: Routes = [
   {path:'feltolt', component:HibaFelvitelComponent},
   {path:'modosit/:id', component:HibaFelvitelComponent},
   {path: "", component: FooldalComponent},
   {path: "chat", component: ChatComponent},
+  {path: "kapcsolat", component: KapcsolatComponent},
   {path:'lista', component:HibaListaComponent}
 ];
 
---
+++ viz/src/app/kapcsolat/kapcsolat.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-kapcsolat',
+  templateUrl: './kapcsolat.component.html',
+  styleUrl: './kapcsolat.component.css'
+})
+export class KapcsolatComponent {
+
+}
