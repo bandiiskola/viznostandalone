05477749a39e0d9e9ff375edea128e3becd964d7
Kovács Csaba <0tac-kovcsa@sztu-02.moriczref.lan>
2024-12-13 09:22:44 +0100

    Létrehozva

---
+++ viz/src/app/fejlec/fejlec.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fejlec',
+  standalone: true,
+  imports: [],
+  templateUrl: './fejlec.component.html',
+  styleUrl: './fejlec.component.css'
+})
+export class FejlecComponent {
+
+}
---
+++ viz/src/app/lablec/lablec.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lablec',
+  standalone: true,
+  imports: [],
+  templateUrl: './lablec.component.html',
+  styleUrl: './lablec.component.css'
+})
+export class LablecComponent {
+
+}
