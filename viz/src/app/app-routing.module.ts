28b6448005661daa6eb185d02e33d51d6cddb0b6
Bere Nándor <0tac-bernan@MORICZREF.LAN>
2024-12-13 12:07:26 +0100

    api

--- viz/src/app/app-routing.module.ts
+++ viz/src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { HibaFelvitelComponent } from './hiba-felvitel/hiba-felvitel.component';
 import { HibaListaComponent } from './hiba-lista/hiba-lista.component';
 import { ChatComponent } from './chat/chat.component';
+import { DiagrammComponent } from './diagramm/diagramm.component';
 
 const routes: Routes = [
   {path:'feltolt', component:HibaFelvitelComponent},
   {path:'modosit/:id', component:HibaFelvitelComponent},
   {path: "", component: FooldalComponent},
   {path: "chat", component: ChatComponent},
+  {path: "diagram", component: DiagrammComponent},
   {path:'lista', component:HibaListaComponent}
 ];
 
---
+++ viz/src/app/diagramm/diagramm.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
+
+@Component({
+  selector: 'app-diagramm',
+  templateUrl: './diagramm.component.html',
+  styleUrl: './diagramm.component.css'
+})
+export class DiagrammComponent {
+  constructor(private apiService: ApiService){}
+  adatok:any[] =[];
+  ngOnInit(): void {
+    const url=`https://berenandor.moriczcloud.hu/viz/diagram`;
+    console.log(url);
+    this.apiService.apiHivas(url).subscribe(
+      response =>{
+        console.log('Api válasz: ',response);
+        this.adatok = response
+      },
+      error =>{
+        console.error('Hiba az API hívás során:')
+      }
+    )
+  }
+}
