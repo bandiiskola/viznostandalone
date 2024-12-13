98311f718ac15436edde9416f3e078f5e06bbc5e
Bere Nándor <0tac-bernan@sztu-08.moriczref.lan>
2024-12-13 09:41:28 +0100

    fasza

--- viz/src/app/app-routing.module.ts
+++ viz/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HibaFelvitelComponent } from './hiba-felvitel/hiba-felvitel.component';
+import { HibaListaComponent } from './hiba-lista/hiba-lista.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:HibaFelvitelComponent},
+  {path:'modosit/:id', component:HibaFelvitelComponent},
+  {path:'lista', component:HibaListaComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
